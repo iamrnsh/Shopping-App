432f76d69978a548659c455ca1b826e4b61dba92
Muhammed Raneesh <rnshrnshrnsh@gmail.com>
2020-03-28 22:32:35 +0530

    This is the first build with basic structure of the app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:koodeapp/screens/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Koode',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        accentColor: Colors.blueGrey,
+      ),
+      home: Home(),
+    );
+  }
+}
